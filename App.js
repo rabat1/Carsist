b3caccfaf1969956e51e1904830d99f08e476deb
rabat1 <57858448+rabat1@users.noreply.github.com>
2021-11-12 23:26:55 +0500

    'packagesVersion'

---
+++ App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+import 'react-native-gesture-handler';
+import React from 'react';
+import {Text} from 'react-native';
+
+
+const App= () => {
+ 
+  return (
+    <Text>App</Text>
+  );
+};
+
+export default App;
